52915816a12ad8c562337df03f7b69035126e2b6
Sunil Kumar <sunilk10099@gmail.com>
2022-08-10 01:23:14 +0530

    Add second widget logic

--- src/components/SecondaryTile.jsx
+++ src/components/SecondaryTile.jsx
@@ -1,22 +1,32 @@
+import { useContext } from "react";
+import DataContext from "./Context/DataContext";
+
 function SecondaryTile() {
+  const { weatherData, loading } = useContext(DataContext);
+
   return (
     <div className="secondary-tile">
-        <div className="max-temp">
+      {loading && <h2>Loading...</h2>}
+      {!loading && (
+        <>
+          <div className="max-temp">
             <p className="title">Max Temp</p>
-            <p className="show">30 *C</p>
-        </div>
-        <div className="min-temp">
+            <p className="show">{weatherData.main.temp_max.toFixed()} &#176;C</p>
+          </div>
+          <div className="min-temp">
             <p className="title">Min Temp</p>
-            <p className="show">30 *C</p>
-        </div>
-        <div className="humidity">
+            <p className="show">{weatherData.main.temp_min.toFixed()} &#176;C</p>
+          </div>
+          <div className="humidity">
             <p className="title">Humidity</p>
-            <p className="show">30 *C</p>
-        </div>
-        <div className="wind">
+            <p className="show">{weatherData.main.humidity.toFixed()}</p>
+          </div>
+          <div className="wind">
             <p className="title">Wind Speed</p>
-            <p className="show">30 *C</p>
-        </div>
+            <p className="show">{weatherData.wind.speed}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
