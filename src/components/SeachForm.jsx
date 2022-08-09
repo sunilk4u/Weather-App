5aa5ccfea18f753df5afcbbcfcc56db8d43c47d1
Sunil Kumar <sunilk10099@gmail.com>
2022-08-09 21:47:13 +0530

    Add search form logic

--- src/components/SeachForm.jsx
+++ src/components/SeachForm.jsx
@@ -1,11 +1,24 @@
+import { useContext, useState } from "react";
+import DataContext from "./Context/DataContext";
+/* eslint-disable */
 function SeachForm() {
+  const [location, setLocation] = useState("");
+  const { fetchData } = useContext(DataContext);
+
+  const submit = (e) => {
+    e.preventDefault();
+    fetchData(location);
+    setLocation("");
+  };
   return (
     <div className="search-form">
-      <form>
+      <form onSubmit={submit}>
         <input
           type="text"
           name="search"
           id="search"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
           placeholder="Enter Location..."
         />
       </form>
