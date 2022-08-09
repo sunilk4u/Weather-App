4a4f3b6c0b3713a71ed6c3d4060f06abe543f375
Sunil Kumar <sunilk10099@gmail.com>
2022-08-09 18:09:36 +0530

    Add Footer Copyright and Poppins Font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import SeachForm from "./components/SeachForm";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <div className="overlay"></div>
       <SeachForm />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="copyright">
+        <a
+          href="https://github.com/sunilk4u/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          &copy; Sunil Kumar
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
