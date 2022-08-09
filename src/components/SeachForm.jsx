e4604bb2bcc0449646a2f968c3b09e4049763ffb
Sunil Kumar <sunilk10099@gmail.com>
2022-08-09 18:15:16 +0530

    Add placeholder text in input

--- src/components/SeachForm.jsx
+++ src/components/SeachForm.jsx
@@ -2,7 +2,12 @@ function SeachForm() {
   return (
     <div className="search-form">
       <form>
-        <input type="text" name="search" id="search" />
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Enter Location..."
+        />
       </form>
     </div>
   );
